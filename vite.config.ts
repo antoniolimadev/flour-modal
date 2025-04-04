4f0b4900530e185baba30092787b57dd92316fa5
Antonio <limaa91@gmail.com>
2025-04-04 21:21:00 +0100

    configure vite.config.ts and package.json to build/publish the library*
    *install vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import dts from "vite-plugin-dts";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), dts({
+    rollupTypes: true,
+    tsconfigPath: "./tsconfig.app.json"
+  })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "FlourModal",
+      fileName: (format) => `flour-modal.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  }
+});
