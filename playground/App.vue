cb9c4d3235d47b16f0f959b3823301c6240e8e4f
Antonio <limaa91@gmail.com>
2025-04-08 21:34:41 +0100

    create playground directory; create child component placeholder

---
+++ playground/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { FlourModal } from '@/components';
+import TheChild from './TheChild.vue';
+
+</script>
+
+<template>
+    <FlourModal msg="Vite + Vue" />
+    <TheChild />
+</template>
