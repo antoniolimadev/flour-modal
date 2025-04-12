13c961fc54fc3e541579f7c9f501820c84b20aae
Antonio <limaa91@gmail.com>
2025-04-12 12:28:59 +0100

    update playground with different modal instances

--- playground/App.vue
+++ playground/App.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
-import { FlourModal } from '@/components';
 import TheChild from './TheChild.vue';
 
 </script>
 
 <template>
-    <FlourModal msg="Vite + Vue" />
-    <TheChild />
+    App.vue here...
+    <br>
+    <button
+        type="button"
+        @click="$flourModal.open({
+            title: 'Is this working?',
+            message: 'Please confirm this is working by pressing the OK button',
+        })"
+    >
+        Open modal
+    </button>
+    <br>
+    <button
+        type="button"
+        @click="$flourModal.open({
+            child: TheChild
+        })"
+    >
+        Open modal with child
+    </button>
+    <br>
+    <button @click="$flourModal.close()">
+        Close modal
+    </button>
 </template>
--- playground/TheChild.vue
+++ playground/TheChild.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { inject } from "vue";
+
+const closeModal = inject<() => void>('closeModal')!;
 
 </script>
 
 <template>
     <div>
-        I am the child
+        <h3>I am the child</h3>
+        <button type="button" @click="closeModal">
+            Close modal
+        </button>
     </div>
 </template>
