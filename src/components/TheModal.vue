336f52c0cea0158429f910d2c7f7daf81a2e7a87
Antonio <limaa91@gmail.com>
2025-04-21 17:03:31 +0100

    create 'error' method which overrides some of the default prop values*
    *add new 'intent' prop which can be used to define the modal's intent e.g. info message, error message, etc

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import '@/styles/the-modal.scss';
-import { onMounted, provide, ref, useTemplateRef } from "vue";
+import {computed, onMounted, provide, ref, useTemplateRef} from "vue";
 import { onClickOutside, onKeyStroke } from "@vueuse/core";
+import type { ModalIntent } from "../types";
+import { MODAL_INTENT } from "../constants";
 
 const props = withDefaults(defineProps<{
     title?: string,
     message?: string,
+    intent: ModalIntent,
     defaultCTA?: boolean,
     destroy: any,
 }>(), {
     title: '',
     message: '',
+    intent: MODAL_INTENT.DEFAULT,
     defaultCTA: true,
     destroy: () => {},
 });
@@ -36,6 +40,16 @@ const target = useTemplateRef<HTMLElement>('target');
 onClickOutside(target, () => closeModal());
 onKeyStroke('Escape', () => closeModal());
 
+const intentClass = computed(() => {
+    switch (props.intent) {
+        default:
+        case MODAL_INTENT.DEFAULT:
+            return 'modal__button--default';
+        case MODAL_INTENT.ERROR:
+            return 'modal__button--error';
+    }
+})
+
 </script>
 
 <template>
@@ -59,7 +73,7 @@ onKeyStroke('Escape', () => closeModal());
                 </div>
 
                 <div v-if="defaultCTA" class="modal__footer">
-                    <button type="button" @click="closeModal" class="modal__button">
+                    <button type="button" @click="closeModal" class="modal__button" :class="intentClass">
                         OK
                     </button>
                 </div>
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,14 @@
 import type { ComponentInternalInstance } from "@vue/runtime-core";
+import { MODAL_INTENT } from "../constants";
+
+export type ModalIntent =
+    MODAL_INTENT.DEFAULT |
+    MODAL_INTENT.ERROR;
 
 export interface ModalOptions {
     title?: string,
     message?: string,
+    intent: ModalIntent,
     defaultCTA?: boolean,
     child?: any
 }
@@ -10,5 +16,6 @@ export interface ModalOptions {
 export interface UseModalInstance {
     createModal(options: ModalOptions): ComponentInternalInstance | null,
     open(options: ModalOptions): ComponentInternalInstance | null | void,
+    error(options: ModalOptions): ComponentInternalInstance | null | void,
     close(): void,
 }
