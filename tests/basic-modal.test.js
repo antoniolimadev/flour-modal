660ff1360dd4e804d6e31f2ae3d94cdf0191437d
Antonio <limaa91@gmail.com>
2025-05-10 16:17:06 +0100

    add more tests for basic functionality

--- tests/basic-modal.test.js
+++ tests/basic-modal.test.js
@@ -1,21 +1,68 @@
-import { expect, test } from 'vitest';
+import { expect, test, describe } from 'vitest';
 import { flushPromises, mount } from '@vue/test-utils';
 import TheModal from "@/components/TheModal.vue";
 
-test('renders title and message when passed as props', async () => {
-  const title = 'Welcome!';
-  const message = 'This is a message';
+describe('basic functionality', () => {
+    const title = 'Welcome!';
+    const message = 'This is a message';
 
-  const wrapper = mount(
-      TheModal, {
-        props: {
-          title,
-          message,
-        }
-      });
+    test('renders title and message when passed as props', async () => {
+        const wrapper = mount(
+            TheModal, {
+                props: {
+                    title,
+                    message,
+                }
+            });
 
-  await flushPromises();
+        await flushPromises();
 
-  expect(wrapper.find('.modal__title').text()).toContain(title);
-  expect(wrapper.find('.modal__message-text').text()).toContain(message);
+        expect(wrapper.find('.modal__title').text()).toContain(title);
+        expect(wrapper.find('.modal__message-text').text()).toContain(message);
+    });
+
+    test('always renders default CTA by default', async () => {
+        const wrapper = mount(
+            TheModal, {
+                props: {
+                    title,
+                    message,
+                }
+            });
+
+        await flushPromises();
+
+        expect(wrapper.find('.modal__button').exists()).toBe(true);
+        expect(wrapper.find('.modal__button').text()).toBe('OK');
+    });
+
+    test('renders custom CTA label when passed as prop', async () => {
+        const wrapper = mount(
+            TheModal, {
+                props: {
+                    title,
+                    message,
+                    defaultCTALabel: 'Got it',
+                }
+            });
+
+        await flushPromises();
+
+        expect(wrapper.find('.modal__button').exists()).toBe(true);
+        expect(wrapper.find('.modal__button').text()).toBe('Got it');
+    });
+    test('does not render default CTA when prop is set to false', async () => {
+        const wrapper = mount(
+            TheModal, {
+                props: {
+                    title,
+                    message,
+                    defaultCTA: false,
+                }
+            });
+
+        await flushPromises();
+
+        expect(wrapper.find('.modal__button').exists()).toBe(false);
+    });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     }
   },
   test: {
-    globals: true,
+    globals: false,
     environment: 'jsdom',
   }
 });
